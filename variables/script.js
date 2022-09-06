12b732a31a180cdb92617ac7b36d917d0e9835f3
spandin <willstesi@gmail.com>
2022-09-06 13:51:07 +0300

    Переменные

---
+++ variables/script.js
@@ -0,0 +1,15 @@
+// learning
+
+let myName = 'Stanislau';
+let userName = myName;
+
+console.log(userName);
+
+let h1Color = 'blue';
+
+let currentYear = '2022';
+
+let firstName = 'Stanislau',
+    lastName = 'Pandzin',
+    age = '23';
+
