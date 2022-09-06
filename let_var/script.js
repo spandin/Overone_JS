afa08100144517fd44c33699cab92d3ecf92d8cd
spandin <willstesi@gmail.com>
2022-09-06 14:07:32 +0300

    Let и var

---
+++ let_var/script.js
@@ -0,0 +1,20 @@
+let car = 'let Honda';
+
+car = 'let Mazda';
+
+console.log(car);
+
+var carV = 'var Honda';
+
+carV = 'var Mazda';
+
+console.log(carV);
+
+
+{ let userName = 'Vanya'; }
+
+console.log(userName);
+
+{ var userNameV = 'Vanya'; }
+
+console.log(userNameV);
