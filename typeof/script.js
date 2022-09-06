bc9423c0843dcd0f21c2c8375f25b2437a1081c6
spandin <willstesi@gmail.com>
2022-09-06 19:07:49 +0300

    Типы данных

---
+++ typeof/script.js
@@ -0,0 +1,24 @@
+let name = 'string'
+
+console.log(typeof name);
+
+let age = 23
+
+console.log(typeof age);
+
+let isLearningJS = true
+
+console.log(typeof isLearningJS);
+
+let info = {
+    name,
+    age,
+    isLearningJS
+}
+
+console.log(typeof info.name);
+
+
+console.log(1 / 0);
+
+console.log(0 / 0);
