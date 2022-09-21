132607d9ef12f1f7c59ee8d559afd859c9a1bcf3
spandin <willstesi@gmail.com>
2022-09-21 17:16:05 +0300

    События клавиатуры

--- keyEvents/script.js
+++ keyEvents/script.js
@@ -1,6 +1,3 @@
-// Создайте на странице 3 блока: event.key, event.which, event.code. 
-// Добавьте слушатель события, который будет отслеживать нажатие клавиши и помещать 
-// в созданные блоки информацию из объекта event.
 const keyBlock = document.querySelector('.key')
 const whichBlock = document.querySelector('.which')
 const codeBlock = document.querySelector('.code')
@@ -8,19 +5,16 @@ const codeBlock = document.querySelector('.code')
 document.addEventListener('keydown', (e) => {
     keyBlock.innerHTML += `<span>${e.key}; </span>`;
     document.key.append(keyBlock);
-    console.log(e);
 })
 
 document.addEventListener('keydown', (e) => {
     whichBlock.innerHTML += `<span>${e.which}; </span>`;
     document.key.append(whichBlock);
-    console.log(e);
 })
 
 document.addEventListener('keydown', (e) => {
     codeBlock.innerHTML += `<span>${e.code}; </span>`;
     document.key.append(codeBlock);
-    console.log(e);
 })
 
 
