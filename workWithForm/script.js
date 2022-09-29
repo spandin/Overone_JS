fdef5bc3b6a1c95e252ab41e9a90c9da95912afe
spandin <willstesi@gmail.com>
2022-09-29 16:14:17 +0300

    Работа с формами

---
+++ workWithForm/script.js
@@ -0,0 +1,22 @@
+const form = document.forms.getInfo
+
+const firstName = form.firstName
+console.log(firstName);
+
+const SecondName = form.SecondName
+console.log(SecondName);
+
+const abountyou = form.abountyou
+console.log(abountyou);
+
+const select = form.marital.options
+console.log(select);
+
+const radio = form.drags
+console.log(radio);
+
+const checkbox = form.confirmation
+console.log(checkbox);
+
+const button = form.submit
+console.log(button);
