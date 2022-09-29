fcf09f5a2188b3d80664e3c7aca1c67529ac989a
spandin <willstesi@gmail.com>
2022-09-29 16:51:31 +0300

    Работа с формами 2

--- workWithForm/script.js
+++ workWithForm/script.js
@@ -1,22 +1,37 @@
 const form = document.forms.getInfo
 
 const firstName = form.firstName
-console.log(firstName);
 
 const SecondName = form.SecondName
-console.log(SecondName);
 
 const abountyou = form.abountyou
-console.log(abountyou);
 
-const select = form.marital.options
-console.log(select);
+const select = form.marital
 
 const radio = form.drags
-console.log(radio);
 
 const checkbox = form.confirmation
-console.log(checkbox);
 
 const button = form.submit
-console.log(button);
+
+checkbox.addEventListener('change', (e) => {
+    if (checkbox.checked == true) {
+        button.style.display = 'block'
+    } else {
+        button.style.display = 'none'
+    }
+})
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let userInfo = document.createElement('div')
+    userInfo.className = 'user-info'
+    userInfo.innerHTML += `<h1>Ваши данные</h1>`
+    userInfo.innerHTML += `<p>Имя: ${firstName.value}</p>`
+    userInfo.innerHTML += `<p>Фамилия: ${SecondName.value}</p>`
+    userInfo.innerHTML += `<p>О себе: ${abountyou.value}</p>`
+    userInfo.innerHTML += `<p>Семейное положение: ${select.value}</p>`
+    userInfo.innerHTML += `<p>Употребляли наркотики: ${radio.value}</p>`
+
+    document.body.append(userInfo)
+})
