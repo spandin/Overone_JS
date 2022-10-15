1db952caaf7fb1cb9a12c3c20b9a2a50932af576
spandin <willstesi@gmail.com>
2022-10-15 21:19:13 +0300

    Работа с датами

--- workWithDate/script.js
+++ workWithDate/script.js
@@ -1,6 +1,17 @@
-const date = new Date()
+const form = document.forms.shelfLifeForm
 
-console.log(date.getFullYear());
-console.log(date.getMonth());
-console.log(date.getDay());
-console.log(date.toISOString());
+const manufacturing = form.manufacturingDate
+const shelf = form.shelfLife
+const btn = form.submit
+
+function getCalc(param1, param2) {
+    return (Date.parse(param2) - Date.parse(param1)) / (1000 * 60 * 60 *24)
+}
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault()
+    const days = getCalc(manufacturing.value, shelf.value)
+    document.body.innerHTML += `До конца срока: ${days}`
+    form.reset()
+})
+
